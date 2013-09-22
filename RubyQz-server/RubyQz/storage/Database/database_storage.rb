92f0edb04cb3185968a056e8d33c3566ae9f27c3
Benoit Goyette <benoitgoyette@gmail.com>
2013-09-22 17:45:57 -0400

    added storage class files

---
+++ RubyQz-server/RubyQz/storage/Database/database_storage.rb
@@ -0,0 +1,14 @@
+module RubyQz
+  module Storage
+    class DatabaseStorage
+      def initialize(config)
+      end
+      
+      def write(topic, yaml_data)
+      end
+      
+      def read(topic)
+      end
+    end
+  end
+end
---
+++ RubyQz-server/RubyQz/storage/Memcache/memcache_storage.rb
@@ -0,0 +1,14 @@
+module RubyQz
+  module Storage
+    class MemcacheStorage
+      def initialize(config)
+      end
+      
+      def write(topic, yaml_data)
+      end
+      
+      def read(topic)
+      end
+    end
+  end
+end
